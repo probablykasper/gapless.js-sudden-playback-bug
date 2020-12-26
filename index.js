5662668683bbf3981f9f74ff1bf3f29e652a8065
Kasper <kasperkh.kh@gmail.com>
2020-12-26 03:23:15 +0100

    Remove stupidity

--- index.js
+++ index.js
@@ -4,20 +4,13 @@ document.querySelector('#button').addEventListener('click', () => {
   if (!window.player) {
     window.player = new Gapless.Queue({
       numberOfTracksToPreload: 2,
-      tracks: [
-        location.origin+'/neo-soul-short.m4a', // 5 seconds long
-        location.origin+'/neo-tokyo-short.m4a', // 38 seconds long
-        location.origin+'/neo-tokyo-short.m4a', // 38 seconds long
-      ]
     })
-    console.log(location.origin+'/neo-tokyo-short.m4a')
-    player.addTrack({trackUrl: location.origin+'/neo-soul-short.m4a'})
-    player.addTrack({trackUrl: location.origin+'/neo-tokyo-short.m4a'})
+    player.addTrack({trackUrl: location.origin+'/neo-soul-short.m4a'}) // 38 seconds long
+    player.addTrack({trackUrl: location.origin+'/neo-tokyo-short.m4a'}) // 38 seconds long
+    player.addTrack({trackUrl: location.origin+'/neo-tokyo-short.m4a'}) // 38 seconds long
   }
-  console.log(player)
   player.play()
   setTimeout(() => {
-    console.log('pause')
     player.pause()
   }, 2000)
 })
